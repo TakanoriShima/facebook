034f1456fa15e2f799bdd05c571b427031b439fa
TakanoriShima <quark2galaxy@gmail.com>
2022-04-07 11:33:49 +0900

    新規投稿、投稿一覧表示完了

---
+++ posts_create.php
@@ -0,0 +1,13 @@
+<?php
+    // (C)
+    require_once 'filters/login_filter.php';
+    require_once 'models/Post.php';
+    $token = session_id();
+    $errors = $_SESSION['errors'];
+    $_SESSION['errors'] = null;
+    $post = $_SESSION['post'];
+    $_SESSION['post'] = null;
+    if($post === null){
+        $post = new Post();
+    }
+    include_once 'views/posts_create_view.php';
